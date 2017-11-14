0a24c34485a32440c1647abd4ef37dffc95412b4
YairTawil <yairtawil@gmail.com>
2017-11-14 15:30:12 +0200

    fix(master): index file

--- index.ts
+++
@@ -1,2 +0,0 @@
-export { DragDropListModule } from './src/drag-drop-list.module';
-export { DragDropListDirective } from './src/drag-drop-list.directive';
---
+++ src/ng-drag-drop-list.ts
@@ -0,0 +1,2 @@
+export * from './drag-drop-list.module';
+export * from './drag-drop-list.directive';
