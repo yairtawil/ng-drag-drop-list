427e8e2dc9f458bfc9fde6e6e6a5b8061e4621af
YairTawil <yairtawil@gmail.com>
2017-12-11 19:09:27 +0200

    fix(module): add CommonModule(inject ElementRef)

--- src/drag-drop-list.module.ts
+++ src/drag-drop-list.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { DragDropListDirective } from './drag-drop-list.directive';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+  imports: [CommonModule],
   declarations: [DragDropListDirective],
   exports: [DragDropListDirective]
 })
